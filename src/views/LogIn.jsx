24595cbb65c4481d7fce468074e7b2e254d6749e
Xavier Gomez <xendke@gmail.com>
2019-03-16 22:02:42 -0400

    create login page

--- src/views/LogIn.jsx
+++ src/views/LogIn.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withFirebase } from '../components/firebase';
+// import './LogIn.scss';
 
-const LogIn = () => {
+const LogIn = (props) => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
 	return (
-		<div>LogIn</div>
+		<form className="section">
+			<div className="field">
+				<label className="label">Email</label>
+				<div className="control">
+					<input className="input" type="text" placeholder="your@email.com" value={email} onChange={(e) => {
+						const val = e.target.value;
+						setEmail(val);
+					}} />
+				</div>
+			</div>
+
+			<div className="field">
+				<label className="label">Password</label>
+				<div className="control">
+					<input className="input" type="password" placeholder="SuperSecret12" value={password} onChange={(e) => {
+						const val = e.target.value;
+						setPassword(val);
+					}} />
+				</div>
+			</div>
+
+
+
+			<div className="field is-grouped">
+				<div className="control">
+					<button
+						className="button is-primary is-inverted"
+						onClick={(e) => {
+							e.preventDefault();
+							props.firebase.doCreateUserWithEmailAndPassword(email, password)
+								.then(authUser => {
+									// this.setState({ ...INITIAL_STATE });
+									console.log(authUser);
+								})
+								.catch(error => {
+									// this.setState({ error });
+									console.log(error);
+								});
+						}}
+					>
+						Log in
+					</button>
+				</div>
+			</div>
+		</form>
 	);
 }
 
-export default LogIn;
+export default withFirebase(LogIn);
