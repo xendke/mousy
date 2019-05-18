378d269e8e790f42d2f7b0fe89e549f658cd00cb
Xavier Gomez <xendke@gmail.com>
2019-05-17 20:50:02 -0400

    homepage css changes

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 
 const Home = () => {
 	return (
-		<div>
-			<div id="main">
-				<h1>don't be shy</h1>
-				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-				<button id="dl">Download</button>
-			</div>
+		<div className="main">
+			<h1>Don't Be Shy</h1>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+			<button className="dl">Download</button>
 		</div>
 	);
 }
