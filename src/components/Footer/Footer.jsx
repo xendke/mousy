04a68d03b5be68d2401b9c72bdb1992c31a6eb78
Juan X Gomez <xendke@gmail.com>
2020-09-04 19:09:19 -0400

    fix footer link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const styles = {
   margin: '5px 0px 25px',
@@ -9,9 +8,9 @@ const styles = {
 const Footer = () => (
   <footer style={styles}>
     Made with ♥ by&nbsp;
-    <Link to="https://twitter.com/xendke" className="has-text-black-ter">
+    <a href="https://twitter.com/xendke" className="has-text-black-ter">
       @xendke
-    </Link>
+    </a>
   </footer>
 )
 
