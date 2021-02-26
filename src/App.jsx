16c372a2de17739b6ce948ab8d31151d2a6db55c
Juan X Gomez <xendke@gmail.com>
2021-02-25 23:42:01 -0500

    redirect public profile to self

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signIn, signOut, setInfo } from '~/redux/actions/user'
 import { withFirebase } from '~/components/firebase'
@@ -22,6 +22,7 @@ class App extends Component {
   }
 
   render() {
+    const { user } = this.props
     return (
       <Router>
         <>
@@ -29,7 +30,16 @@ class App extends Component {
           <Route path="/" exact component={Home} />
           <Route path="/me" exact component={Profile} />
           <Route path="/account/:tab?" exact component={Account} />
-          <Route path="/shy/:userId" component={Profile} />
+          <Route
+            path="/shy/:userId"
+            component={({ match, ...props }) =>
+              user.auth.uid === match.params.userId ? (
+                <Redirect to="/me" />
+              ) : (
+                <Profile match={match} {...props} />
+              )
+            }
+          />
           <Route path="/login" exact component={Login} />
           <Route path="/join" exact component={Join} />
           <Footer />
