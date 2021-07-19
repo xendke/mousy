157c28ee577bc007fd27d0fc3334e22d08792913
Juan X Gomez <xendke@gmail.com>
2021-07-18 22:41:44 -0400

    add custom styles to select

--- src/components/InterestsSelect/InterestsSelect.jsx
+++ src/components/InterestsSelect/InterestsSelect.jsx
@@ -6,7 +6,21 @@ const components = {
   DropdownIndicator: null,
 }
 
-const createOption = (label: string) => ({
+const customStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    boxShadow: 'inset 0 0.0625em 0.125em rgb(10 10 10 / 5%)',
+    backgroundColor: '#F8F8FF',
+    ...(state.isFocused
+      ? {
+          borderColor: '#3273dc',
+          boxShadow: '0 0 0 0.125em rgb(50 115 220 / 25%)',
+        }
+      : {}),
+  }),
+}
+
+const createOption = (label) => ({
   label,
   value: label,
 })
@@ -18,7 +32,7 @@ const isDuplicate = (options, needle) =>
 
 const MAX = 10
 
-export default class InterestsSelect extends Component<*, State> {
+export default class InterestsSelect extends Component {
   constructor(props) {
     super(props)
     const { defaultInterests } = this.props
@@ -28,17 +42,17 @@ export default class InterestsSelect extends Component<*, State> {
     }
   }
 
-  handleChange = (value: any) => {
+  handleChange = (value) => {
     const { getInterests } = this.props
     this.setState({ value })
     getInterests(value.map(toLabels))
   }
 
-  handleInputChange = (inputValue: string) => {
+  handleInputChange = (inputValue) => {
     this.setState({ inputValue })
   }
 
-  handleKeyDown = (event: SyntheticKeyboardEvent<HTMLElement>) => {
+  handleKeyDown = (event) => {
     const { inputValue, value } = this.state
     const { getInterests } = this.props
     if (!inputValue) return
@@ -72,6 +86,7 @@ export default class InterestsSelect extends Component<*, State> {
     const { id: inputId } = this.props
     return (
       <CreatableSelect
+        styles={customStyles}
         components={components}
         inputValue={inputValue}
         inputId={inputId}
